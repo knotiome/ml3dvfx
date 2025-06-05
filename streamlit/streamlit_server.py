a0febd82b42b8269d2dbb863a4ddab33bca6b9c7
I-d-as <philetmannion@gmail.com>
2025-06-05 09:27:05 -0400

    update

--- streamlit/streamlit_server.py
+++ streamlit/streamlit_server.py
@@ -5,15 +5,17 @@ import pickle
 from sklearn.ensemble import RandomForestClassifier
 
 
-st.write("""
-Hello this app is now ran      
-""") 
+# st.write("""
+# Hello this app is now ran      
+# """) 
 
-st.sidebar.header("User input data features")
-st.sidebar.markdown("""
-[CSV inpute file: ]
-- This is just a **TEST**
-""")
+# st.sidebar.header("User input data features")
+# st.sidebar.markdown("""
+# [CSV inpute file: ]
+# - This is just a **TEST**
+# """)
+
+# UI -----------------
 
 uploaded_file = st.sidebar.file_uploader("CSV File: ", type=["csv"])
 if uploaded_file is not None:
@@ -23,8 +25,8 @@ else:
     def user_input_features():
         SepalLengthCm = st.sidebar.slider('SepalLengthCm', 4.3, 8.0, 6.0)
         SepalWidthCm = st.sidebar.slider('SepalWidthCm', 2.0, 5.0, 3.0)
-        PetalLengthCm = st.sidebar.('PetalLengthCm', 1.0, 7.0, 4.0)
-        PetalWidthCm = st.sidebar.('PetalWidthCm', 0.1, 3.0, 2.0)
+        PetalLengthCm = st.sidebar.slider('PetalLengthCm', 1.0, 7.0, 4.0)
+        PetalWidthCm = st.sidebar.slider('PetalWidthCm', 0.1, 3.0, 2.0)
         data = {'SepalLengthCm': SepalLengthCm,
                 'SepalWidthCm': SepalWidthCm,
                 'PetalLengthCm': PetalLengthCm,
@@ -35,10 +37,22 @@ else:
     input_df = user_input_features()
         
 iris_raw = pd.read_csv('iris.csv')
-iris = iris_raw.drop(['Species', 'Id'])
+iris = iris_raw.drop(['Species', 'Id'], axis=1)
+df = pd.concat([input_df, iris], axis=0)
 
+df = df[:1]
 
+st.subheader("User input features")
 
+if uploaded_file is not None:
+    st.write(df)
+else:
+    st.write("using sample data, upload a CSV file if needed")
+    st.write(df)
 
+# model ----------------
+
+load_clf = pickle.load(open('iris_clf.pkl', 'rb'))
+prediction = load_clf.predict(df)
 
 

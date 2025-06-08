a8682e937c012aee8ceae6e540fde44d27b0fba5
rmannion <rmannion3d@gmail.com.com>
2025-06-08 04:02:50 -0400

    trained model

--- foxes_dogs/create_training_data.py
+++ foxes_dogs/create_training_data.py
@@ -5,7 +5,7 @@ import os
 from tqdm import tqdm
 import cv2
 
-DATADIR = "/home/phile/development/Data/Animal Image Dataset-Cats, Dogs, and Foxes"
+DATADIR = "/home/phile/.cache/kagglehub/datasets/snmahsa/animal-image-dataset-cats-dogs-and-foxes/versions/1/Animal Image Dataset-Cats, Dogs, and Foxes"
 CATEGORIES = ["dog","fox"]
 IMG_SIZE = 50
 
@@ -13,14 +13,14 @@ training_data = []
 
 def create_tr_data():
     for category in CATEGORIES:
-        path = os.path.join(DATADIR,category)
-        class_num = CATEGORIES.index(category)
+        path = os.path.join(DATADIR, category)
+        clas_num = CATEGORIES.index(category)
 
         for img in tqdm(os.listdir(path)):
             try:
-                img_array = cv2.imread(os.path.join(path,img),cv2.IMREAD_GRAYSCALE)
-                resized_array = cv2.resize(img_arr,(IMG_SIZE,IMG_SIZE))
-                training_data.append([resized_array,class_num])
+                img_arr = cv2.imread(os.path.join(path,img), cv2.IMREAD_GRAYSCALE)
+                resized_arr = cv2.resize(img_arr, (IMG_SIZE, IMG_SIZE))
+                training_data.append([resized_arr, clas_num])
             except Exception as e:
                 pass
 
@@ -32,14 +32,14 @@ random.shuffle(training_data)
 X = []
 y = []
 
-for features,labels in training_data:
+for features, labels in training_data:
     X.append(features)
     y.append(labels)
 
-X = np.array(X).reshape(-1,IMG_SIZE,IMG_SIZE,1)
+X = np.array(X).reshape(-1, IMG_SIZE, IMG_SIZE, 1)
 
 with open("X.pkl","wb") as Xfile:
     pickle.dump(X, Xfile)
 
 with open("y.pkl","wb") as yfile:
-    pickle.dump(y,yfile)
+    pickle.dump(y, yfile)

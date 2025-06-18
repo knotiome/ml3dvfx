92fdc4a9efa2dcb678c9b9965aeb6c2647d0414b
Ryan <philetmannion@gmail.com>
2025-06-18 03:40:47 -0400

    video pose detection

---
+++ mediapipe/pose_detection.py
@@ -0,0 +1,40 @@
+import cv2
+import numpy as np
+import mediapipe as mp
+
+mp_draw = mp.solutions.drawing_utils
+mp_styles = mp.solutions.drawing_styles
+mp_pose = mp.solutions.pose
+
+source = "dancer.mp4"
+
+cap = cv2.VideoCapture(source)
+
+with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+    while cap.isOpened():
+        ret, image = cap.read()
+
+        if ret:
+            image.flags.writeable = False
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            results = pose.process(image)
+
+            image.flags.writeable = True
+
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            mp_draw.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                   landmark_drawing_spec=mp_styles.get_default_pose_landmarks_style())
+            
+            cv2.imshow("Pose", image)
+
+            if cv2.waitKey(5) == ord('q'):
+                break
+
+        else:
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+

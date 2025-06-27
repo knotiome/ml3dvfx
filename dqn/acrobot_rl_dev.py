778f53ee6535dddaedd186cd57471f319ded1254
Ryan <philetmannion@gmail.com>
2025-06-27 11:12:56 -0400

    started

---
+++ dqn/acrobot_rl_dev.py
@@ -0,0 +1,25 @@
+import gym
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import random
+from collections import deque
+
+env = gym.make('Acrobot-v1', render_mode='human')
+               
+class DQN(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super(DQN, self).__init__()
+        self.fc1 = nn.Linear(input_dim, 128)
+        self.fc2 = nn.Linear(128,128)
+        self.fc3 = nn.Linear(128, output_dim)
+
+    def forward(self, x):
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = self.fc3(x)
+
+        return x
+    
+    

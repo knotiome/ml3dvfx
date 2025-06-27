cb8f24971766c431d3192837a00a1eeef53d8595
rmannion <rmannion3d@gmail.com.com>
2025-06-27 15:19:48 -0400

    trained model

---
+++ dqn/train_acrobot.py
@@ -0,0 +1,13 @@
+import gym
+from stable_baselines3 import DQN
+from stable_baselines3.common.vec_env import DummyVecEnv
+
+env = gym.make('Acrobot-v1')
+env = DummyVecEnv([lambda: env])
+
+# Train
+model = DQN('MlpPolicy', env, verbose=1)
+model.learn(total_timesteps=100000)
+
+# Save model
+model.save('dqn_acrobot')

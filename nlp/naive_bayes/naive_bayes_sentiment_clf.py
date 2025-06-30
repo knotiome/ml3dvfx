d979c9ba47c7d8ce4ff5355655db559ee8200b1a
rmannion <rmannion3d@gmail.com.com>
2025-06-29 22:43:52 -0400

    started

---
+++ nlp/naive_bayes/naive_bayes_sentiment_clf.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import classification_report, accuracy_score
+
+
+
+
+
+
+
+
+
+
+
+
+

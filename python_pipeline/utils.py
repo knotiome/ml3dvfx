a3660b0079563338310e99f2139fa9d6f1b560c2
rmannion <rmannion3d@gmail.com.com>
2025-07-13 10:49:17 -0400

    progress

--- python_pipeline/utils.py
+++ python_pipeline/utils.py
@@ -5,4 +5,10 @@ def save_model(model, target_dir, model_name):
     target_dir_path = Path(target_dir)
     target_dir_path.mkdir(parents=True, exist_ok=True)
 
-    assert model
+    assert model_name.endswith(".pth") or model_name.endswith(".pt"), "model_name should end with .pth or .pt"
+
+
+    model_path = target_dir_path / model_name
+
+    torch.save(model.state_dict(), model_path)
+    
